be05d533060281b3de2a902df9bf420fb2cf4997
Daisy Molving <daisymolving@gmail.com>
2016-12-12 14:25:26 +0000

    tests functionality of board class that it knows about wins, draws and available cells

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,4 +1,105 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Board {
-    public static String[] grid = new String[9];
+
+    private List<String> cells = Arrays.asList("", "", "", "", "", "", "", "", "");
+
+    public List<String> getCells() {
+        return cells;
+    }
+
+    public boolean gridIsEmpty() {
+       List<String> currentBoard = getCells();
+       for (int i = 0; i < currentBoard.size(); i++) {
+           if (currentBoard.get(i) != "") {
+               return false;
+           } else {
+               continue;
+           }
+       } return true;
+    }
+
+    public List<String> markCell(int index, String marker) {
+        List<String> currentGrid = getCells();
+        currentGrid.set(index, marker);
+        return currentGrid;
+    }
+
+    public boolean availableCell(int index) {
+        List<String> currentGrid = getCells();
+        String cell = (String) currentGrid.get(index);
+        return cell.isEmpty();
+    }
+
+    public boolean win() {
+        List<List<String>> sequences = getSequences();
+        if (allMarked(sequences)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean draw() {
+        List<String> currentBoard = getCells();
+        for (int i = 0; i < currentBoard.size(); i++) {
+            if (currentBoard.get(i) == "") {
+                return false;
+            } else {
+                continue;
+            }
+        } return true;
+    }
+
+    public List<List<String>> getRows() {
+        List<List<String>> rows = new ArrayList<List<String>>();
+        rows.add(getCells().subList(0, 3));
+        rows.add(getCells().subList(3, 6));
+        rows.add(getCells().subList(6, 9));
+        return rows;
+    }
+
+    public List<List<String>> getColumns() {
+        List<List<String>> columns = new ArrayList<List<String>>();
+        columns.add(Arrays.asList(getCells().get(0), getCells().get(3), getCells().get(6)));
+        columns.add(Arrays.asList(getCells().get(1), getCells().get(4), getCells().get(7)));
+        columns.add(Arrays.asList(getCells().get(2), getCells().get(5), getCells().get(8)));
+        return columns;
+    }
+
+    public List<List<String>> getDiagonals() {
+        List<List<String>> diagonals = new ArrayList<List<String>>();
+        diagonals.add(Arrays.asList(getCells().get(0), getCells().get(4), getCells().get(8)));
+        diagonals.add(Arrays.asList(getCells().get(2), getCells().get(4), getCells().get(6)));
+        return diagonals;
+    }
+
+    private boolean allMarked(List<List<String>> sequences) {
+        if (sequences.contains(markedSequenceO()) || sequences.contains(markedSequenceX())){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private List<String> markedSequenceX() {
+        List<String> markedSequenceX = Arrays.asList("x", "x", "x");
+        return markedSequenceX;
+    }
+
+    private List<String> markedSequenceO() {
+        List<String> markedSequenceO = Arrays.asList("o", "o", "o");
+        return markedSequenceO;
+    }
+
+    private List<List<String>> getSequences(){
+        List<List<String>> sequences = new ArrayList<List<String>>();
+        sequences.addAll(getRows());
+        sequences.addAll(getColumns());
+        sequences.addAll(getDiagonals());
+        return sequences;
+    }
 
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,12 +1,148 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
 
-    @Test
-    public void knowANewBoardGridHasNineCells() {
-        assertEquals(new Board().grid.length, 9);
+    private Board board = new Board();
+    private Board numberedBoard() {
+        board.markCell(0, "1");
+        board.markCell(1, "2");
+        board.markCell(2, "3");
+        board.markCell(3, "4");
+        board.markCell(4, "5");
+        board.markCell(5, "6");
+        board.markCell(6, "7");
+        board.markCell(7, "8");
+        board.markCell(8, "9");
+        return board;
     }
 
+    @Test
+    public void knowsANewBoardGridHasNineCells() {
+        assertEquals(9, new Board().getCells().size());
+    }
+
+    @Test
+    public void knowsANewBoardGridIsEmpty() {
+        assertTrue(board.gridIsEmpty());
+    }
+
+    @Test
+    public void marksACell() {
+        List<String> currentGrid = Arrays.asList(
+                "x", "", "",
+                "", "", "",
+                "", "", "");
+        ArrayList markedBoard = new ArrayList(currentGrid);
+        assertEquals(markedBoard, board.markCell(0, "x"));
+    }
+
+    @Test
+    public void knowsAMarkedBoardIsNotEmpty() {
+        board.markCell(0, "x");
+        assertFalse(board.gridIsEmpty());
+    }
+
+    @Test
+    public void knowsIfACellIsEmpty() {
+        board.markCell(0, "x");
+        assertFalse(board.availableCell(0));
+        assertTrue(board.availableCell(1));
+    }
+
+    @Test
+    public void findsBoardRows() {
+        List<List<String>> rows = Arrays.asList(
+                Arrays.asList("1", "2", "3"),
+                Arrays.asList("4", "5", "6"),
+                Arrays.asList("7", "8", "9")
+        );
+        assertEquals(rows, numberedBoard().getRows());
+    }
+
+    @Test
+    public void findsBoardColumns() {
+        List<List<String>> columns = Arrays.asList(
+                Arrays.asList("1", "4", "7"),
+                Arrays.asList("2", "5", "8"),
+                Arrays.asList("3", "6", "9")
+        );
+        assertEquals(columns, numberedBoard().getColumns());
+    }
+
+    @Test
+    public void findsBoardDiagonals() {
+        List<List<String>> diagonals = Arrays.asList(
+                Arrays.asList("1", "5", "9"),
+                Arrays.asList("3", "5", "7")
+        );
+        assertEquals(diagonals, numberedBoard().getDiagonals());
+    }
+
+    @Test
+    public void recognisesARowWin() {
+        board.markCell(0, "x");
+        board.markCell(1, "x");
+        board.markCell(2, "x");
+        assertTrue(board.win());
+    }
+
+    @Test
+    public void recognisesANonWin() {
+        board.markCell(0, "x");
+        board.markCell(2, "x");
+        assertFalse(board.win());
+        board.markCell(0, "x");
+        board.markCell(1, "o");
+        board.markCell(2, "x");
+        assertFalse(board.win());
+    }
+
+    @Test
+    public void recognisesAColumnWin() {
+        board.markCell(0, "o");
+        board.markCell(3, "o");
+        board.markCell(6, "o");
+        assertTrue(board.win());
+    }
+
+    @Test
+    public void recognisesADiagonalWin() {
+        board.markCell(0, "o");
+        board.markCell(4, "o");
+        board.markCell(8, "o");
+        assertTrue(board.win());
+    }
+
+    @Test
+    public void recognisesADraw() {
+        board.markCell(0, "x");
+        board.markCell(1, "o");
+        board.markCell(2, "x");
+        board.markCell(3, "x");
+        board.markCell(4, "o");
+        board.markCell(5, "x");
+        board.markCell(6, "o");
+        board.markCell(7, "x");
+        board.markCell(8, "o");
+        assertTrue(board.draw());
+    }
+
+    @Test
+    public void recognisesANonDraw() {
+        board.markCell(0, "x");
+        board.markCell(1, "o");
+        board.markCell(2, "x");
+        board.markCell(3, "x");
+        board.markCell(5, "x");
+        board.markCell(6, "o");
+        assertFalse(board.draw());
+    }
 }
