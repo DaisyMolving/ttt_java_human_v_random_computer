0992b4f9da81e126697c016d4aeb801faa076d6c
Daisy Molving <daisymolving@gmail.com>
2016-12-16 13:24:41 +0000

    improves CliDisplay tests, removes @After

--- src/test/java/CliDisplayTest.java
+++ src/test/java/CliDisplayTest.java
@@ -10,25 +10,17 @@ public class CliDisplayTest {
 
     private final ByteArrayOutputStream output = new ByteArrayOutputStream();
     private final ByteArrayInputStream input = new ByteArrayInputStream("string data\n".getBytes());
-    private PrintStream systemOutput;
 
     @Before
     public void beginIOStreams() {
-        systemOutput = System.out;
         System.setOut(new PrintStream(output));
         System.setIn(input);
-    }
-
-    @After
-    public void wipeStreams() {
-        System.setOut(systemOutput);
-        System.setIn(System.in);
     }
 
     @Test
     public void cliDisplayPrintsOutputToConsole() {
         Display cliOutput = new CliDisplay();
-        cliOutput.sendMessageToDisplay("Hi There!");
+        cliOutput.sendToTheDisplay("Hi There!");
         assertEquals("Hi There!", output.toString());
     }
 
