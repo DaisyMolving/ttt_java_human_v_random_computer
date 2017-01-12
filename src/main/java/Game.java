341fe319b857d47afe4ec6ee614e4db9d29724e3
Daisy Molving <daisymolving@gmail.com>
2017-01-12 13:48:59 +0000

    bones of new game set up and loop

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,13 +1,33 @@
 public class Game {
 
     private Board board;
-    private CliDisplay cliDisplay;
+    private Display display;
     private Messenger messenger;
 
     public Game(Board board, CliDisplay cliDisplay, Messenger messenger) {
         this.board = board;
-        this.cliDisplay = cliDisplay;
+        this.display = display;
         this.messenger = messenger;
     }
 
+    public void playNew() {
+        setUpNewGame();
+        while (inProgress()) {
+            //request move
+            //switch players
+        } //display end result
+    }
+
+    public void setUpNewGame() {
+        display.sendToTheDisplay(messenger.welcomePlayers());
+        display.getResponse(messenger.requestGameType());
+    }
+
+    public boolean inProgress() {
+        return !isOver();
+    }
+
+    public boolean isOver() {
+        return board.draw() || board.win();
+    }
 }
