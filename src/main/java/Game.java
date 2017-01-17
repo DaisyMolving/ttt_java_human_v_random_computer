4b04a1ba104957d41f74a2533f9518256185f313
Daisy Molving <daisymolving@gmail.com>
2017-01-17 12:00:24 +0000

    adds random computer player to game options, switches players successfully

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,8 +6,8 @@ public class Game {
     private Board board;
     private Display display;
     private Messenger messenger;
-    private Player playerOne;
-    private Player playerTwo;
+    public Player playerOne;
+    public Player playerTwo;
 
     public Game(Board board, Display display, Messenger messenger) {
         this.board = board;
@@ -25,18 +25,35 @@ public class Game {
 
     public void setUpNewGame() {
         display.sendToTheDisplay(messenger.welcomePlayers());
-        display.sendToTheDisplay(messenger.askGameType());
+        String gameType = display.getResponse(messenger.askGameType());
+        createPlayers(gameType);
+    }
+
+    public void createPlayers(String gameType) {
+        if (gameType.equals("a")) {
+            createHVHGame();
+        } else if (gameType.equals("b")) {
+            createHVCGame();
+        }
+    }
+
+    public void createHVHGame() {
         playerOne = new HumanPlayer("Player 1", "x");
         playerTwo = new HumanPlayer("Player 2", "o");
     }
 
+    public void createHVCGame() {
+        playerOne = new HumanPlayer("Player 1", "x");
+        playerTwo = new RandomComputerPlayer("Player 2", "o");
+    }
+
     public Board requestMove() {
         display.sendToTheDisplay(messenger.setUpBoard(board));
-        return Player.makeMove(board);
+        return playerOne.makeMove(board);
     }
 
     public void switchPlayers() {
-        HumanPlayer temporaryPlayer = new HumanPlayer(playerOne.getName(), playerOne.getMarker());
+        Player temporaryPlayer = playerOne;
         playerOne = playerTwo;
         playerTwo = temporaryPlayer;
     }
